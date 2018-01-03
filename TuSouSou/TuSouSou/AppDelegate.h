baea368db9651fb4cb6c26c0923456f7395d2bf3
ljf651218196 <651218196@qq.com>
2018-01-03 15:54:10 +0800

    初始

---
+++ TuSouSou/TuSouSou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TuSouSou
+//
+//  Created by newmac on 2018/1/3.
+//  Copyright © 2018年 GuoYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TuSouSou/TuSouSou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TuSouSou
+//
+//  Created by newmac on 2018/1/3.
+//  Copyright © 2018年 GuoYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
